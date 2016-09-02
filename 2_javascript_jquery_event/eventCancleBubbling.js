75a302f0bb78159abdf69d7fed37e96b782d367c
seungyoub <seungyoub.kim@traport.com>
2016-09-02 12:18:42 +0900

    chore(eventCancleBubbling.js): 주석추가

--- 2_javascript_jquery_event/eventCancleBubbling.js
+++ 2_javascript_jquery_event/eventCancleBubbling.js
@@ -4,6 +4,7 @@ window.onload = function() {
   var c = document.getElementById('c')
 
   //원래 이벤트리스너의 익명함수의 첫번째 인자로 event객체를 전달하기로 약속되어있다.
+  //event를 이용해 현재 발생한 이벤트에 대한 여러 정보들을 알 수 있다.
   //허나 event객체를 사용하지 않을거라면 생략해도 무관
   a.addEventListener('click', function(event) {
     event.stopPropagation();
