0df27fc38ec8671be2502d42d3b43f73b5f23ff9
seungyoub <seungyoub.kim@traport.com>
2016-09-02 10:15:00 +0900

    chore(*): 주석 추가

--- 1_html_css_javascript/jsEx.js
+++ 1_html_css_javascript/jsEx.js
@@ -1,4 +1,7 @@
+//DOM을 로드하며 외부에 참조하고 있는 이미지등까지 모두 로딩이 완료되면 함수 실행
 window.onload = function() {
+  //브라우저의 Console창에 로그
   console.log("window.onload console log");
+  //알림창
   alert("window.onload ALERT 경고창");
 };
--- 2_javascript_jquery_event/test.js
+++ 2_javascript_jquery_event/test.js
@@ -1,9 +1,12 @@
 function titleClick() {
+  //innerHTML은 해당 태그안의 내용을 가져옴
   alert(document.getElementById('title').innerHTML);
 }
 
 $(function(){
+  //copyButton이라는 아이디를 가진 태그가 클릭되었을때 실행
   $('#copyButton').click(function() {
+    //copyInput이라는 클래스를 가진 태그의 value에 input이라는 아이디를 가진 태그의 value를 대입
     $('.copyInput').val($('#input').val());
   });
 });
